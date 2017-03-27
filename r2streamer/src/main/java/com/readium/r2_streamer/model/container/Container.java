fce39e9e9e22166990c9b205f727bcd6c9657e8b
Vikas Hiran <vikas@mobisys.in>
2017-03-27 20:23:13 +0530

    unwanted code removed, changed package name from com.codetoart.r2_streamer to com.readium.r2_streamer

---
+++ r2streamer/src/main/java/com/readium/r2_streamer/model/container/Container.java
@@ -0,0 +1,15 @@
+package com.readium.r2_streamer.model.container;
+
+import java.io.InputStream;
+
+/**
+ * Created by Shrikant Badwaik on 24-Jan-17.
+ */
+
+public interface Container {
+    String rawData(String relativePath) throws NullPointerException;
+
+    int rawDataSize(String relativePath);
+
+    InputStream rawDataInputStream(String relativePath) throws NullPointerException;
+}
---
+++ r2streamer/src/main/java/com/readium/r2_streamer/util/Constants.java
@@ -0,0 +1,9 @@
+package com.readium.r2_streamer.util;
+
+/**
+ * Created by Shrikant Badwaik on 17-Feb-17.
+ */
+
+public class Constants {
+    public static final String EPUBTITLE = "BARRETT_GUIDE.epub";
+}
